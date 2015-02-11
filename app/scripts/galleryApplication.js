022d4cbbba4a9e3da2e969f9f3a546ba76e573e1
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-02-11 00:32:38 +0000

    Update remote PouchDB URL to production address
    The database is read-only so there's no harm in using this
    instance even for development purposes.  Still, it would be
    good to make it configurable on an environment basis.

--- app/scripts/galleryApplication.js
+++ app/scripts/galleryApplication.js
@@ -7,7 +7,7 @@
     GalleryController,
     RemoteDatabaseManager) {
   var dbName = "natalie_gallery",
-    remoteDbUrl = "http://natalie_www:topSecret@127.0.0.1:5984/natalie_gallery",
+    remoteDbUrl = "http://natalie_www:topSecret@natalia.dns4e.net:5984/natalie_gallery",
     db = new PouchDB( dbName ),
     remoteDb = new PouchDB( remoteDbUrl ),
     remoteDatabaseManager = new RemoteDatabaseManager( remoteDb, db );

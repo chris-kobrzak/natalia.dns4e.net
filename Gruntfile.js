b0faa736ba723027b69d7c1596f1a1ff4f2f2385
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-02-11 00:35:53 +0000

    Release v0.1.0
    Adds `grunt-bump` development dependency.

--- Gruntfile.js
+++ Gruntfile.js
@@ -407,6 +407,23 @@ module.exports = function (grunt) {
       }
     },
 
+    bump: {
+      options: {
+        files: ["package.json"],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: "Release v%VERSION%",
+        commitFiles: ["package.json"],
+        createTag: true,
+        tagName: "v%VERSION%",
+        tagMessage: "Version %VERSION%",
+        push: false,
+        pushTo: "upstream",
+        gitDescribeOptions: "--tags --always --abbrev=1 --dirty=-d",
+        globalReplace: false
+      }
+    },
+
     // Run some tasks in parallel to speed up build process
     concurrent: {
       server: [

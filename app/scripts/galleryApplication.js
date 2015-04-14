7c64694522b4c7f3767464d27226f3f16774bbf1
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-04-14 22:43:36 +0100

    New URL of CouchDB database (and the website)
    This was required because dns4e.net have recently announced they
    would be shutting down the service.

--- app/scripts/galleryApplication.js
+++ app/scripts/galleryApplication.js
@@ -7,7 +7,7 @@
     GalleryController,
     RemoteDatabaseManager) {
   var dbName = "natalie_gallery",
-    remoteDbUrl = "http://natalie_www:topSecret@natalia.dns4e.net:5984/natalie_gallery",
+    remoteDbUrl = "http://natalie_www:topSecret@tunia.duckdns.org:5984/natalie_gallery",
     db = new PouchDB( dbName ),
     remoteDb = new PouchDB( remoteDbUrl ),
     remoteDatabaseManager = new RemoteDatabaseManager( remoteDb, db );
